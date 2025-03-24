edf2bba8f82e8720b436fee30a5e693dc2020d72
MohammedYasserr <bmbmyasser@gmail.com>
2025-03-24 23:42:36 +0100

    Added the env file and adding dependencies in the requirements.txt

---
+++ scripts/rag.py
@@ -0,0 +1,18 @@
+from langchain.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain import utils
+import os
+from pydantic import BaseModel
+from typing import List, Dict
+import getpass
+import json
+import sys
+import bs4
+from langchain import hub
+from langchain.graphs import START,StateGraph
+from typing_extensions import List, TypedDict
